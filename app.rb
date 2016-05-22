74795398ff94e876a9721af7f788788ad2be16b8
Hironori Kobayashi <hironori.kobayashi.pingpong@gmail.com>
2016-05-22 19:05:01 +0900

    update app

--- app.rb
+++ app.rb
@@ -2,24 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  branch = "master"
+  branch = "20160521_hkobayashi_test"
   app_dir = "/var/www/dev-lavida"
 
-  if branch === "master" then
-    return "not a git repository." if `cd #{app_dir} && ls -la | grep ".git"`.empty?
-    Dir.chdir(app_dir){
-      return `git pull origin #{branch}`
-    }
-    return  "git pull " << branch << " to " << app_dir
-
-  elsif !(name = branch[/\d{4,10}_(hkobayashi|kyaegashi|hsuzuki|hmurano|zynas)/, 1]).nil?
-    return "Not a git repository." if `cd #{app_dir}_#{name} && ls -la | grep ".git"`.empty?
-    Dir.chdir(app_dir << "_" << name){
-      `git checkout -b #{branch}` if `git branch | grep "*" | grep #{branch}`.empty?
-      return `git pull origin #{branch}`
-    }
-
-  else
+  if !(name = branch[/\d{4,10}_(hkobayashi|kyaegashi|hsuzuki|hmurano|zynas)/, 1]).nil?
+    app_dir = app_dir << "_" << name
+  elsif branch != "master" then
+    # not deploy branch
     return "branch name is not matched. " << branch
   end
+
+  # repository  check
+  return "not a git repository." if `cd #{app_dir} && ls -la | grep ".git"`.empty?
+
+  # checkout brach and pull branch
+  Dir.chdir(app_dir){
+    `git checkout -b #{branch}` if `git branch | grep "*" | grep #{branch}`.empty?
+    return "dir: " << app_dir << "\n" << "branch: " << branch << "\n" << `git pull origin #{branch}`
+  }
+
 end
