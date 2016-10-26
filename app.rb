c09dc5a7e5675dd1ebd9b38102ba948cb6c300b4
Hironori Kobayashi <hironori.kobayashi.pingpong@gmail.com>
2016-10-26 21:10:14 +0900

    deploy cs2

--- app.rb
+++ app.rb
@@ -15,30 +15,46 @@ class App < Sinatra::Base
     logger.info http_headers
     payload = JSON.parse(params[:payload])
     branch = payload["ref"][/refs\/heads\/(.*)/, 1]
-    app_dir = "/var/www/dev-lavida"
-
-    if !(name = branch[/\d{4,10}_(hkobayashi|kyaegashi|hsuzuki|hmurano|zynas|tyamamoto)/, 1]).nil?
-      app_dir = app_dir << "_" << name
-    elsif branch != "master" then
-      # not deploy branch
-      return "branch name is not matched. " << branch
-    end
-
-    # repository  check
-    return "not a git repository." if `cd #{app_dir} && ls -la | grep ".git"`.empty?
-
-    result = ""
-    # checkout brach and pull branch
-    Dir.chdir(app_dir) do
-      `git checkout -b #{branch}` if `git branch | grep "*" | grep #{branch}`.empty?
-      result = `git pull origin #{branch}`
-    end
-
-    return "dir: " << app_dir << "\n" << "branch: " << branch << "\n" << result
+    return App.deploy(branch)
   end
 
-  get '/hello' do
-    'Hello!'
+  get '/test' do
+    branch = request["branch"]
+    return App.deploy(branch)
+  end
+
+  def App.deploy(branch)
+    script_dir = Dir.pwd
+    vhost_dir = "/var/www/dev.comsuite.jp/"
+
+    # check branch (exclude develop,master)
+    excludeBranch = ["develop", "master"]
+    if excludeBranch.include?(branch)
+      return "This branch is not deployed. branch: #{branch}"
+    end
+
+    app_dir = vhost_dir + branch
+    result = ""
+    # dir check branch
+    if Dir.exist?(app_dir)
+      # exists
+      Dir.chdir(app_dir) do
+        result = `git pull origin #{branch}`
+      end
+    else
+      # not exists
+      Dir.chdir(vhost_dir) do
+        `git clone -b #{branch} git@github.com:e-life/cs2.git ./#{branch}`
+        `chmod -R 777 #{app_dir}/application/var/`
+        `chmod -R 777 #{app_dir}/public/_var/`
+        `sed -i -e "s/dev-cs2\.comsuite\.jp/#{branch}\.dev\.comsuite\.jp/g" #{app_dir}/application/configs/application.ini`
+        `cp #{script_dir}/template/.htaccess #{app_dir}/public/`
+        `sed -i -e "s/BRANCH_NAME/#{branch}/g" #{app_dir}/public/.htaccess`
+        result = "clone and checkout #{branch}"
+      end
+    end
+
+    return "dir: #{app_dir}\nbranch: #{branch}\result: #{result}"
   end
 
 end
