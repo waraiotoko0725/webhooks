f2f88abd1f91fcd5a6fc41464715ffa7e22a5586
Hironori Kobayashi <hironori.kobayashi.pingpong@gmail.com>
2016-11-11 18:19:52 +0900

    dev.community.lavida用カスタマイズ

--- app.rb
+++ app.rb
@@ -25,12 +25,16 @@ class App < Sinatra::Base
 
   def App.deploy(branch)
     script_dir = Dir.pwd
-    vhost_dir = "/var/www/dev.comsuite.jp/"
+    vhost_dir = "/var/www/dev-community-lavida_virtual/"
 
-    # check branch (exclude develop,master)
-    excludeBranch = ["develop", "master"]
+    # check branch (exclude develop)
+    excludeBranch = ["develop"]
     if excludeBranch.include?(branch)
       return "This branch is not deployed. branch: #{branch}"
+    end
+
+    if branch == "master"
+      vhost_dir = "/var/www/dev-community-lavida/"
     end
 
     app_dir = vhost_dir + branch
@@ -44,10 +48,10 @@ class App < Sinatra::Base
     else
       # not exists
       Dir.chdir(vhost_dir) do
-        `git clone -b #{branch} git@github.com:e-life/cs2.git ./#{branch}`
+        `git clone -b #{branch} git@github.com:e-life/lavida-community.git ./#{branch}`
         `chmod -R 777 #{app_dir}/application/var/`
         `chmod -R 777 #{app_dir}/public/_var/`
-        `sed -i -e "s/dev-cs2\.comsuite\.jp/#{branch}\.dev\.comsuite\.jp/g" #{app_dir}/application/configs/application.ini`
+        `sed -i -e "s/dev\.community\.lavida\.jp/#{branch}\.dev\.community\.lavida\.jp/g" #{app_dir}/application/configs/application.ini`
         `cp #{script_dir}/template/.htaccess #{app_dir}/public/`
         `sed -i -e "s/BRANCH_NAME/#{branch}/g" #{app_dir}/public/.htaccess`
         result = "clone and checkout #{branch}"
